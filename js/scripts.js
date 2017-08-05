08aba2b48b20d524c114881509f387e988b997ff
dtrwoga <dtrwoga@gmail.com>
2017-08-05 22:01:15 +0200

    poprawione nawiasy

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,13 @@ var i,j,k;
 
 function drawTree(k) {
   	for (i=0; i<k; i++) {
-    	var star =("");
+    	var star ="";
     	for (j=0; j<=i; j++) {
-       		var star = (star += "* "); 
+       		var star = star += "* "; 
     	}  
-    	var spacja =("");
+    	var spacja ="";
   		for (j=k-1; j>i; j--) {
-       		var spacja = (spacja += " "); 
+       		var spacja = spacja += " "; 
       	}
 	console.log(spacja + star);
 	}
